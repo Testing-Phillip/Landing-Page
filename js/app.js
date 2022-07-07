d33cfd596730e9ae1a9c4855d15256a57ac021af
Phillip Gooden <phillipgooden@gmail.com>
2022-07-07 14:03:32 -0500

    js fixes

--- js/app.js
+++ js/app.js
@@ -1,34 +1,36 @@
-const NAV_BAR = document.querySelector('#navbar__list');
-// console.log(NAV_BAR);//!VERIFY
-const NAV_SECTIONS = document.querySelectorAll('section');
-console.log(NAV_SECTIONS);//!VERIFY
+const NAV_BAR = document.querySelector('#navbar__list'); // creates const for dynamic navbar
+const NAV_SECTIONS = document.querySelectorAll('section'); //creates const for section element data
 
 const nav_menu_items = () => {
-  let navMenu = '';
+	let navMenu = '';
 
-NAV_SECTIONS.forEach(section => {
-
-     const SECTION_NUMBER = section.id;
-    //  console.log(SECTION_NUMBER);//!VERIFY
-     const SECTION_ATTRIBUTE = section.dataset.nav;
-     navMenu += `<li> <a class="dynamic_menu" href="#${SECTION_NUMBER}">${SECTION_ATTRIBUTE}</a></li>`;   
-   });
-NAV_BAR.innerHTML=navMenu;
+	NAV_SECTIONS.forEach(section => {
+		const SECTION_NUMBER = section.id;
+		const SECTION_ATTRIBUTE = section.dataset.nav;
+		navMenu += `<li> <a class="dynamic_menu" href="#${SECTION_NUMBER}">${SECTION_ATTRIBUTE}</a></li>`;
+	});
+	NAV_BAR.innerHTML = navMenu;
 };
-
 nav_menu_items();
 
-const OBSERVER = new IntersectionObserver(entries => {
-  // console.log(entries); //!VERIFY
-  entries.forEach(entry => {
-    entry.target.classList.toggle("active", entry.isIntersecting);
-    // if (entry.isIntersecting) OBSERVER.unobserve(entry.target)//!VERIFY
-  });
-},
-{
-  threshold: 0.7
+const NAV_LINKS = document.querySelectorAll(".dynamic_menu"); // creates const for indiviual links inside dynamic navbar
+NAV_LINKS.forEach(NAV_LINK => NAV_LINK.addEventListener("click", function(e) {
+	e.preventDefault();
+	const JUMP = e.currentTarget.getAttribute("href");
+	window.scrollTo({ //smooths USERS link selection
+		top: document.querySelector(JUMP).offsetTop,
+		behavior: "smooth"
+	});
+}));
+
+const OBSERVER = new IntersectionObserver(entries => { //clicking on nav menu items sets active class
+	entries.forEach(entry => {
+		entry.target.classList.toggle("active", entry.isIntersecting);
+	});
+}, {
+	threshold: 0.75 //toggles active class if 75% of element is visible on screen
 });
 
-NAV_SECTIONS.forEach(NAV_SECTIONS => {
-  OBSERVER.observe(NAV_SECTIONS);
+NAV_SECTIONS.forEach(NAV_SECTIONS => { //loops through section data for changes in intersection
+	OBSERVER.observe(NAV_SECTIONS);
 });
